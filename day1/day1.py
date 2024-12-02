95749c9ac5a440025dd0d711e81414deb5af9597
lennoncc <lencruz@ucdavis.edu>
2024-12-01 23:11:13 -0800

    Part 1 done; star achieved

--- day1/day1.py
+++ day1/day1.py
@@ -1,1 +1,16 @@
 # Start of day 1!
+# Read in file input
+f = open("day1.in", "r+")
+# Put into list to make easier to parse through
+lines = f.readlines()
+# Remove newlines
+lines = [line.strip() for line in lines]
+# Separate into separate lists
+list1 = [int(line.split()[0]) for line in lines]
+list2 = [int(line.split()[1]) for line in lines]
+list1.sort()
+list2.sort()
+sumdistance = 0
+for i in range(len(list1)):
+  sumdistance += abs(list1[i] - list2[i])
+print(sumdistance)
